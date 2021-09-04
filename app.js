bd36b69084bc8554969baf53cb245963645addef
Tushar Jain <54166594+tusharjain88954@users.noreply.github.com>
2021-09-04 22:43:28 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+require("./models/db");
+require("./config/passportConfig");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const path = require("path");
+const rtsIndex = require("./routes/index.router");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+app.use("/", rtsIndex);
+app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+// error handler
+app.use((err, req, res, next) => {
+  // handling nicely mongodb validation errors
+  if (err.name === "ValidationError") {
+    var valErrors = [];
+    Object.keys(err.errors).forEach((key) =>
+      valErrors.push(err.errors[key].message)
+    );
+    res.status(422).send(valErrors);
+  }
+});
+
+// start server
+app.listen(PORT, () =>
+  console.log(`Server started at port : ${process.env.PORT}`)
+);
---
+++ models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database is connected successfully");
+  })
+  .catch((err) => {
+    console.log("Some error occured during connection");
+  });
+
+require("./user.model");
